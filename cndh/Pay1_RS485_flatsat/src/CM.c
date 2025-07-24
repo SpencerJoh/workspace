5222d4e1f66817b2b1cf7a375e41dbe1baae4c57
SeungwonJo <winwon1@inha.edu>
2025-07-24 16:47:29 +0900

    250724 17

--- cndh/Pay1_RS485_flatsat/src/CM.c
+++ cndh/Pay1_RS485_flatsat/src/CM.c
@@ -306,13 +306,24 @@ void RS485_ProcessEvents(UART_HandleTypeDef *huart)
 
 void testing_TC(void)
 {
-
-
+    /* =========================================== 1 =========================================== */
     /* for testing CM TC */
     build_tx_buffer_CMTC();
     if (HAL_UART_Transmit_IT(&huart6, tx_buff_CMTC, sizeof(tx_buff_CMTC)) != HAL_OK)
     {
         ES_TRACE_DEBUG("TX Failed: CubeMag TC\n");
+    }
+
+    RS485_RxMode();
+    if (HAL_UART_Receive(&huart6, rs485_rx_buffer, MAX_PACKET_LENGTH, 50) == HAL_OK)          // goal for timeout is 50ms
+    {
+        ES_TRACE_DEBUG("!!!!!! RX Failed !!!!!!\n");
+    }
+    HAL_UART_AbortReceive(&huart6);
+    if (data[5] == 0x00) {
+        ES_TRACE_DEBUG("CubeMag Config Command(ID: 60) Successful\r\n");
+    } else {
+        ES_TRACE_DEBUG("CubeMag Config Command(ID: 60) Failed with error code: %d\r\n", data[5]);
     }
 
 
@@ -321,14 +332,26 @@ void testing_TC(void)
     uint8_t power_off_CW1[] = {ESC, SOM, 60, MainOBC, ReactionWheel1, 0x00, ESC, EOM};
     uint8_t power_on_CW2[] = {ESC, SOM, 60, MainOBC, ReactionWheel2, 0x01, ESC, EOM};
     uint8_t power_off_CW2[] = {ESC, SOM, 60, MainOBC, ReactionWheel2, 0x00, ESC, EOM};
-    
-    float rpm = 100.0f;
-    uint8_t tx_buff_CW_TC64[11] = {0x1F, 0x80, 64, 0xF0, 0x02};
-    memcpy(&tx_buff_CW_TC64[5], &rpm, sizeof(float));  // 5~8에 rpm 삽입
-    tx_buff_CW_TC64[9] = 0x1F;
-    tx_buff_CW_TC64[10] = 0xFF;
-    
+    // uint8_t tx_buff_CW1_TC[11] = {
+    //     0x1F, 0x80, 64, 0xF0, 0x02, 0x00, 0x00, 0xFA, 0x43, 0x1F, 0xFF
+    // };
+    // uint8_t tx_buff_CW2_TC[11] = {
+    //     0x1F, 0x80, 64, 0xF0, 0x03, 0x00, 0x00, 0xFA, 0x43, 0x1F, 0xFF
+    // };
 
+    float rpm = 100.0f;
+    uint8_t tx_buff_CW1_TC64[11] = {0x1F, 0x80, 64, 0xF0, 0x02};
+    memcpy(&tx_buff_CW1_TC64[5], &rpm, sizeof(rpm));  // 5~8에 rpm 삽입
+    tx_buff_CW1_TC64[9] = 0x1F;
+    tx_buff_CW1_TC64[10] = 0xFF;
+    
+    uint8_t tx_buff_CW2_TC64[11] = {0x1F, 0x80, 64, 0xF0, 0x03};
+    memcpy(&tx_buff_CW2_TC64[5], &rpm, sizeof(rpm));  // 5~8에 rpm 삽입
+    tx_buff_CW2_TC64[9] = 0x1F;
+    tx_buff_CW2_TC64[10] = 0xFF;
+
+    /* =========================================== 2 =========================================== */
+    RS485_TxMode();
     if (HAL_UART_Transmit_IT(&huart6, power_on_CW1, sizeof(power_on_CW1)) != HAL_OK)
     {
         ES_TRACE_DEBUG("TX Failed: CubeWheel1 TC\n");
@@ -340,52 +363,84 @@ void testing_TC(void)
         ES_TRACE_DEBUG("!!!!!! RX Failed !!!!!!\n");
     }
     HAL_UART_AbortReceive(&huart6);
-    
-
-
-
-
-    if ((rs485_rx_buffer[5] == 0x00) || (rs485_rx_buffer[5] == 0x01)) {
-        memcpy(&MotorPower1, &rs485_rx_buffer[5], sizeof(MotorPower1));
-        ES_TRACE_DEBUG(
-            "Reaction Wheel 1 Motor Power => Switch: %s\r\n",
-            MotorPower1.MotorPowerSwitch ? "ON" : "OFF"
-        );
+    if (data[5] == 0x00) {
+        ES_TRACE_DEBUG("CubeWheel1 TC Command(ID: 60) Successful\r\n");
     } else {
-        ES_TRACE_DEBUG("CubeWheel1 TC Command(ID: 64) Unknown response code: %d\r\n", rs485_rx_buffer[5]);
+        ES_TRACE_DEBUG("CubeWheel1 TC Command(ID: 60) Failed with error code: %d\r\n", data[5]);
     }
 
 
 
-
-
-
-    
+    /* =========================================== 3 =========================================== */
     RS485_TxMode();
-
-    rpm = 200.0f;  // Change RPM for testing
-    memcpy(&tx_buff_CWTC[5], &rpm, sizeof(float));  // 5~8에 rpm 삽입
-
     if (HAL_UART_Transmit_IT(&huart6, power_on_CW2, sizeof(power_on_CW2)) != HAL_OK)
     {
         ES_TRACE_DEBUG("TX Failed: CubeWheel2 TC\n");
     }
 
     RS485_RxMode();
-
     if (HAL_UART_Receive(&huart6, rs485_rx_buffer, MAX_PACKET_LENGTH, 50) == HAL_OK)          // goal for timeout is 50ms
     {
         ES_TRACE_DEBUG("!!!!!! RX Failed !!!!!!\n");
     }
+    HAL_UART_AbortReceive(&huart6);
+    if (data[5] == 0x00) {
+        ES_TRACE_DEBUG("CubeWheel2 TC Command(ID: 60) Successful\r\n");
+    } else {
+        ES_TRACE_DEBUG("CubeWheel2 TC Command(ID: 60) Failed with error code: %d\r\n", data[5]);
+    }
 
 
+    /* =========================================== 4 =========================================== */
+    RS485_TxMode();
+    if (HAL_UART_Transmit_IT(&huart6, tx_buff_CW1_TC64, sizeof(tx_buff_CW_TC64)) != HAL_OK)
+    {
+        ES_TRACE_DEBUG("TX Failed: CubeWheel1 TC for 100RPM\n");
+    }
+    RS485_RxMode();
+    if (HAL_UART_Receive(&huart6, rs485_rx_buffer, MAX_PACKET_LENGTH, 50) == HAL_OK)          // goal for timeout is 50ms
+    {
+        ES_TRACE_DEBUG("!!!!!! RX Failed !!!!!!\n");
+    }
+    HAL_UART_AbortReceive(&huart6);
+    if (data[5] == 0x00) {
+        ES_TRACE_DEBUG("CubeWheel1 TC Command(ID: 64) Successful\r\n");
+    } else if (data[5] == 0x01) {
+        ES_TRACE_DEBUG("CubeWheel1 TC Command(ID: 64) Failed with error code: %d\r\n", data[5]);
+    } else {
+        ES_TRACE_DEBUG("CubeWheel1 TC Command(ID: 64) Unknown response code: %d\r\n", data[5]);
+    }
+
+
+    /* =========================================== 5 =========================================== */
+    RS485_TxMode();
+
+
+
+
+    /* =========================================== 6 =========================================== */
+    /* =========================================== 7 =========================================== */
+    /* =========================================== 8 =========================================== */
+    /* =========================================== 9 =========================================== */
+    /* =========================================== 10 =========================================== */
+
+
+
+    // if ((rs485_rx_buffer[5] == 0x00) || (rs485_rx_buffer[5] == 0x01)) {
+    //     memcpy(&MotorPower1, &rs485_rx_buffer[5], sizeof(MotorPower1));
+    //     ES_TRACE_DEBUG(
+    //         "Reaction Wheel 1 Motor Power => Switch: %s\r\n",
+    //         MotorPower1.MotorPowerSwitch ? "ON" : "OFF"
+    //     );
+    // } else {
+    //     ES_TRACE_DEBUG("CubeWheel1 TC Command(ID: 64) Unknown response code: %d\r\n", rs485_rx_buffer[5]);
+    // }
 }
 
 
 
-// uint8_t tx_buff_CWTC[11] = {
-//     0x1F, 0x80, 64, 0xF0, 0x02, 0x00, 0x00, 0xFA, 0x43, 0x1F, 0xFF
-// };
+
+
 
 
 
